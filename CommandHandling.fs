d225beffe67c6f27a3fc6349ed76fe0da5452c03
James Freiwirth <james@perfectshuffle.co.uk>
2013-02-13 15:44:53 +0000

    Initial commit

---
+++ CommandHandling.fs
@@ -0,0 +1,22 @@
+﻿namespace PerfectShuffle.EventSourcing
+
+type CmdOutput<'TEvent> =
+| Success of seq<'TEvent>
+| Failure of exn
+
+type CommandHandler<'TCmd,'TEvent> = 'TCmd -> Async<CmdOutput<'TEvent>>
+type EventSerializer<'TEvent> = seq<'TEvent> -> Async<unit>
+type CommandProcessor<'TCmd, 'TEvent, 'TExternalState>(readModel:IReadModel<'TEvent,'TExternalState>, cmdHandler:CommandHandler<'TCmd,'TEvent>, serialize:EventSerializer<'TEvent>) =
+  
+  let processCmd cmd =
+    async {
+      let! output = cmdHandler cmd
+    
+      match output with
+      | Success evts ->
+        do! serialize evts
+        readModel.Apply evts
+      | Failure ex -> raise ex
+    }
+    
+  member this.Process (cmd:'TCmd) = processCmd cmd |> Async.RunSynchronously
---
+++ ReadModel.fs
@@ -0,0 +1,48 @@
+﻿namespace PerfectShuffle.EventSourcing
+  open Microsoft.FSharp.Control
+
+  type Agent<'t> = MailboxProcessor<'t>
+ 
+  type IReadModel<'TEvent, 'TState> =
+    abstract member Apply : events:seq<'TEvent> -> unit
+    abstract member CurrentState : unit -> 'TState
+    abstract member CurrentStateAsync : unit -> Async<'TState>
+
+  type private Msg<'TEvent, 'TExternalState> =
+    | Update of List<'TEvent>
+    | CurrentState of AsyncReplyChannel<'TExternalState>     
+
+  type ReadModel<'TEvent, 'TInternalState, 'TExternalState>(initialState, evtAccumulator, expose) = 
+    let agent =
+      Agent<Msg<'TEvent, 'TExternalState>>.Start(fun inbox ->
+
+        let rec loop pendingEvents (internalState:'TInternalState) =
+          match pendingEvents with
+          | [] ->
+            async {            
+            let! msg = inbox.Receive()
+            
+            match msg with
+            | Update(evts) ->              
+              return! loop evts internalState  
+            | CurrentState replyChannel ->
+                let currentState = expose internalState
+                replyChannel.Reply currentState
+                return! loop pendingEvents internalState          
+            }
+
+          | firstEvent::remainingEvents ->
+            let newState = evtAccumulator internalState firstEvent
+            loop remainingEvents newState                   
+
+        loop [] initialState
+        )
+
+    interface IReadModel<'TEvent, 'TExternalState> with  
+      member __.Apply evts =
+        let msg = Update(evts |> Seq.toList)
+        agent.Post msg
+       
+      member __.CurrentState() = agent.PostAndReply(fun reply -> CurrentState(reply))
+      member __.CurrentStateAsync() = agent.PostAndAsyncReply(fun reply -> CurrentState(reply))
+   
