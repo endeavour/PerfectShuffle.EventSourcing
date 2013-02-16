fda6b9d4a86e21d90c278aee58d93cf5561ada4c
James Freiwirth <james@perfectshuffle.co.uk>
2013-02-16 10:16:13 +0000

    Add command handling interfaces

--- PerfectShuffle.EventSourcing/CommandHandling.fs
+++ PerfectShuffle.EventSourcing/CommandHandling.fs
@@ -4,8 +4,19 @@ type CmdOutput<'TEvent> =
 | Success of seq<'TEvent>
 | Failure of exn
 
+
 type CommandHandler<'TCmd,'TEvent> = 'TCmd -> Async<CmdOutput<'TEvent>>
+
+// For C# and autofac friendliness
+type ICommandHandler<'TCmd,'TEvent> =
+  abstract Handle : cmd:'TCmd -> Async<CmdOutput<'TEvent>>
+
 type EventSerializer<'TEvent> = seq<'TEvent> -> Async<unit>
+
+type ICommandProcessor<'TCmd> =
+  abstract Process : cmd:'TCmd -> unit
+  abstract ProcessAsync : cmd:'TCmd -> Async<unit>
+
 type CommandProcessor<'TCmd, 'TEvent, 'TExternalState>(readModel:IReadModel<'TEvent,'TExternalState>, cmdHandler:CommandHandler<'TCmd,'TEvent>, serialize:EventSerializer<'TEvent>) =
   
   let processCmd cmd =
@@ -19,5 +30,6 @@ type CommandProcessor<'TCmd, 'TEvent, 'TExternalState>(readModel:IReadModel<'TEv
       | Failure ex -> raise ex
     }
     
-  member this.Process (cmd:'TCmd) = processCmd cmd |> Async.RunSynchronously
-  member this.ProcessAsync (cmd:'TCmd) = processCmd cmd
+  interface ICommandProcessor<'TCmd> with
+    member this.Process (cmd:'TCmd) = processCmd cmd |> Async.RunSynchronously
+    member this.ProcessAsync (cmd:'TCmd) = processCmd cmd
