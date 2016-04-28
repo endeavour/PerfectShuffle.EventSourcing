2e056fcf16eeee18d19cef1c3bbb6c8b01e13835
James Freiwirth <james@perfectshuffle.co.uk>
2016-04-28 21:49:54 +0100

    More work on azure

--- PerfectShuffle.EventSourcing.AzureTableStorage/EventRepository.fs
+++ PerfectShuffle.EventSourcing.AzureTableStorage/EventRepository.fs
@@ -18,7 +18,30 @@ module EventStore =
       let tableClient = cloudStorageAccount.CreateCloudTableClient()
       tableClient
 
-/// Implementation of an event repository that connects to Greg Young's EventStore (www.geteventstore.com)
+type EventTypeWrapper(tableEntity:DynamicTableEntity) =
+
+  member this.EventId
+    with get() = tableEntity.["EventId"].GuidValue
+    and set value = tableEntity.["EventId"].GuidValue <- value
+
+  member this.EventNumber
+    with get() = tableEntity.["EventNumber"].Int32Value
+    and set value = tableEntity.["EventNumber"].Int32Value <- value
+
+  member this.EventType
+    with get() = tableEntity.["EventType"].StringValue
+    and set value = tableEntity.["EventType"].StringValue <- value
+
+  member this.Payload
+    with get() = tableEntity.["Payload"].StringValue
+    and set value = tableEntity.["Payload"].StringValue <- value
+
+  member this.EventTimestamp
+    with get() = tableEntity.["EventTimestamp"].DateTime
+    and set value = tableEntity.["EventTimestamp"].DateTime <- value
+
+
+/// Implementation of an event repository that connects to Azure Table Storage
 type EventRepository<'TEvent>(azureStorageAccountName:string, storageCredentials:Auth.StorageCredentials, tableName:string, serializer : Serialization.IEventSerializer<'TEvent>) =
     
   let tableClient =
@@ -26,32 +49,11 @@ type EventRepository<'TEvent>(azureStorageAccountName:string, storageCredentials
     let tableClient = cloudStorageAccount.CreateCloudTableClient()
     tableClient
 
-//  let tableEntity =
-//    { new ITableEntity with
-//        member __.PartitionKey
-//          with get() = failwith "foo"
-//          and set (value) = failwith "foo"
-//        member __.ETag
-//          with get() = failwith "foo"
-//          and set (value) = failwith "foo"
-//        member __.RowKey
-//          with get() = failwith "foo"
-//          and set (value) = failwith "foo"
-//        member __.Timestamp
-//          with get() = failwith "foo"
-//          and set (value) = failwith "foo"
-//        member __.WriteEntity(ctx) = failwith "foo"
-//      }
-
-  let unpack (e:ResolvedEvent) =      
-    serializer.Deserialize({TypeName = e.Event.EventType; Payload = e.Event.Data})
-
+  let table = tableClient.GetTableReference(tableName)
 
   let getTableRows () = asyncSeq {
-    let table = tableClient.GetTableReference(tableName)
-    let! created = table.CreateIfNotExistsAsync() |> Async.AwaitTask
-    let fetchAll = TableQuery<_>()
     
+    let fetchAll = TableQuery<_>()
     let! firstPage = table.ExecuteQuerySegmentedAsync<DynamicTableEntity>(fetchAll, null) |> Async.AwaitTask
     for r in firstPage.Results do
       yield r    
@@ -63,20 +65,65 @@ type EventRepository<'TEvent>(azureStorageAccountName:string, storageCredentials
         yield r
   }
 
+  let load() =
+    getTableRows()
+    |> AsyncSeq.map (fun x ->
+      let wrapped = EventTypeWrapper x
+      serializer.Deserialize {TypeName = wrapped.EventType; Payload = System.Text.Encoding.UTF8.GetBytes wrapped.Payload}
+      )
+
   let commit (concurrencyCheck:WriteConcurrencyCheck) (evts:EventWithMetadata<'TEvent>[]) = async {
       let eventsData =
         evts |> Array.map (fun e ->
           let serializedEvent = serializer.Serialize e
-          let metaData = [||] : byte array
-          new EventData(Guid.NewGuid(), serializedEvent.TypeName, true, serializedEvent.Payload, metaData)          
+          // TODO: Maybe the serializer should return text instead?
+          let jsonPayload = System.Text.Encoding.UTF8.GetString serializedEvent.Payload
+          Guid.NewGuid(), serializedEvent.TypeName, serializedEvent.Payload
         )
 
-      let expectedVersion =
+//      let expectedVersion =
+//        match concurrencyCheck with
+//        | Any -> ExpectedVersion.Any
+//        | NoStream -> ExpectedVersion.NoStream
+//        | EmptyStream -> ExpectedVersion.EmptyStream
+//        | NewEventNumber n -> n - 1
+
+      let saveEvents (firstEventNumber : Option<int>) =
+        let batchOperation : TableBatchOperation =
+          let operations =
+            evts
+            |> Seq.mapi (fun i evt ->
+              let entity = DynamicTableEntity()
+              let wrapped = EventTypeWrapper(entity)
+              wrapped.EventId <- Nullable(evt.Id)
+              wrapped.EventNumber <- firstEventNumber + i
+              wrapped.EventType <- evt.Timestamp
+              )
+          let op = TableBatchOperation()
+          seq {
+            yield TableOperation.Insert entity
+          } |> Seq.iter op.Add
+
+        table.ExecuteBatchAsync(operation) |> Async.AwaitTask
+        table.ExecuteAsync()
+        async {
+          table
+        }
+
+      let foo =
         match concurrencyCheck with
-        | Any -> ExpectedVersion.Any
-        | NoStream -> ExpectedVersion.NoStream
-        | EmptyStream -> ExpectedVersion.EmptyStream
-        | NewEventNumber n -> n - 1
+        | NoStream ->
+          async {
+            let! tableExists = table.ExistsAsync() |> Async.AwaitTask
+            if tableExists
+              then
+                return failwith "Table should not exist already"
+              else
+                let! created = table.CreateAsync() |> Async.AwaitTask
+                return failwith "DIE"
+          }
+        | EmptyStream | NewEventNumber 0 ->
+        | NewEventNumber n ->
 
       let! result =
         conn.AppendToStreamAsync(streamId, expectedVersion, eventsData)
--- PerfectShuffle.EventSourcing/EventStore.fs
+++ PerfectShuffle.EventSourcing/EventStore.fs
@@ -6,9 +6,13 @@ module Store =
   open System.Net
 
   type WriteConcurrencyCheck =
+  /// This disables the optimistic concurrency check.
   | Any
+  /// this specifies the expectation that target stream does not yet exist.
   | NoStream
+  /// this specifies the expectation that the target stream has been explicitly created, but does not yet have any user events written in it.
   | EmptyStream
+  /// Any other integer value	The event number that you expect the stream to currently be at.
   | NewEventNumber of int
 
   type WriteResult =
