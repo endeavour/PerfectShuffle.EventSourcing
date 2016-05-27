253e74e7acfd23f7f0fbb4038e2d95a6551cb72d
danielr <danielr@digiterre.com>
2016-05-27 10:59:34 +0100

    Move stream into the EventSourcing project

--- PerfectShuffle.EventSourcing.SqlStorage/EventRepository.fs
+++ PerfectShuffle.EventSourcing.SqlStorage/EventRepository.fs
@@ -155,28 +155,5 @@ module SqlStorage =
       member x.SaveEvents(streamName: string) (concurrencyCheck: WriteConcurrencyCheck) (evts: EventToRecord []): Async<WriteResult> = 
         commit streamName concurrencyCheck evts
 
-  type Stream<'event>(streamName:string, serializer : Serialization.IEventSerializer<'event>, dataProvider:IDataProvider) =
   
-    let eventsFrom version =
-      dataProvider.GetStreamEvents streamName version
-      |> AsyncSeq.map (fun rawEvent ->
-            let event = serializer.Deserialize({TypeName = rawEvent.Metadata.TypeName; Payload = rawEvent.Payload})                        
-            { RecordedEvent = event; Metadata = rawEvent.Metadata}
-          )
-
-    let commit concurrencyCheck (evts:EventToRecord<'event>[]) =
-      let rawEvents =
-        evts
-        |> Array.map(fun evt ->
-          let serializedEvent = evt.EventToRecord |> serializer.Serialize         
-          let rawEvent : EventToRecord = { SerializedEventToRecord = serializedEvent; Metadata = evt.Metadata}
-          rawEvent
-          ) 
-      
-      dataProvider.SaveEvents streamName concurrencyCheck rawEvents
-
-    interface IStream<'event> with
-      member __.FirstVersion = 1L
-      member __.EventsFrom version = eventsFrom version
-      member __.Save evts concurrencyCheck = commit concurrencyCheck evts
 
---
+++ PerfectShuffle.EventSourcing/Stream.fs
@@ -0,0 +1,29 @@
+﻿namespace PerfectShuffle.EventSourcing
+
+open Store
+open FSharp.Control
+
+type Stream<'event>(streamName:string, serializer : Serialization.IEventSerializer<'event>, dataProvider:IDataProvider) =
+  
+    let eventsFrom version =
+      dataProvider.GetStreamEvents streamName version
+      |> AsyncSeq.map (fun rawEvent ->
+            let event = serializer.Deserialize({TypeName = rawEvent.Metadata.TypeName; Payload = rawEvent.Payload})                        
+            { RecordedEvent = event; Metadata = rawEvent.Metadata}
+          )
+
+    let commit concurrencyCheck (evts:EventToRecord<'event>[]) =
+      let rawEvents =
+        evts
+        |> Array.map(fun evt ->
+          let serializedEvent = evt.EventToRecord |> serializer.Serialize         
+          let rawEvent : EventToRecord = { SerializedEventToRecord = serializedEvent; Metadata = evt.Metadata}
+          rawEvent
+          ) 
+      
+      dataProvider.SaveEvents streamName concurrencyCheck rawEvents
+
+    interface IStream<'event> with
+      member __.FirstVersion = 1L
+      member __.EventsFrom version = eventsFrom version
+      member __.Save evts concurrencyCheck = commit concurrencyCheck evts
